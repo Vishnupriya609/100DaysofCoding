e2d05ef9be6918382d79defc034de72ab3ce1bd1
Vishnupriya609 <vishnupriyapallerla@gmail.com>
2026-05-25 14:50:14 +0530

    Add files via upload

---
+++ Day 7/dataTypesRange.java
@@ -0,0 +1,14 @@
+// in programmatic to find range
+public class dataTypesRange {
+
+	public static void main(String[] args) {
+		// using wrapper class ONLY FOR NUMBERS
+		System.out.println("Maximum value of Byte : " + Byte.MAX_VALUE);
+		System.out.println("Minimum value of Byte: " + Byte.MIN_VALUE);
+		System.out.println("Maximum value of Short : " +Short.MAX_VALUE);
+		System.out.println("Minimum value of Short: " +Short.MIN_VALUE);
+		System.out.println("Maximum value of Integer : " +Integer.MAX_VALUE);
+		System.out.println("Minimum value of Integer: " +Integer.MIN_VALUE);
+	}
+
+}
