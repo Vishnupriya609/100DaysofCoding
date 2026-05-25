4fa8c89f068cfa74745dfbc9efade26deee9a7f8
Vishnupriya609 <vishnupriyapallerla@gmail.com>
2026-05-25 12:11:55 +0530

    Update dataTypesSize.java

--- Day 7/dataTypesSize.java
+++ Day 7/dataTypesSize.java
@@ -3,10 +3,10 @@ public class dataTypesSize {
 
 	public static void main(String[] args) {
 		
-		// using wrapper classes
-		System.out.println("Short Size(bits) : " + Short.SIZE);
-		System.out.println("Float Size(bytes) : " + Float.SIZE/8);
-		System.out.println("Integer Size(bits) : " + Integer.SIZE/8);
+		// using wrapper classes  
+		System.out.println("Short Size(bits) : " + Short.SIZE); // output : Short Size(bits) : 16
+		System.out.println("Float Size(bytes) : " + Float.SIZE/8); // Output : Float Size(bytes) : 4
+		System.out.println("Integer Size(bits) : " + Integer.SIZE/8);  // output ; Integer Size(bits) : 4
 		
 
 	}
