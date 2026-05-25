0fbd4264bb67a7603d4d0956cf4ab462972e5e20
Vishnupriya609 <vishnupriyapallerla@gmail.com>
2026-05-25 14:52:57 +0530

    Update dataTypesRange.java

--- Day 7/dataTypesRange.java
+++ Day 7/dataTypesRange.java
@@ -3,12 +3,12 @@ public class dataTypesRange {
 
 	public static void main(String[] args) {
 		// using wrapper class ONLY FOR NUMBERS
-		System.out.println("Maximum value of Byte : " + Byte.MAX_VALUE);
-		System.out.println("Minimum value of Byte: " + Byte.MIN_VALUE);
-		System.out.println("Maximum value of Short : " +Short.MAX_VALUE);
-		System.out.println("Minimum value of Short: " +Short.MIN_VALUE);
-		System.out.println("Maximum value of Integer : " +Integer.MAX_VALUE);
-		System.out.println("Minimum value of Integer: " +Integer.MIN_VALUE);
+		System.out.println("Maximum value of Byte : " + Byte.MAX_VALUE); // Maximum value of Byte : 127
+		System.out.println("Minimum value of Byte: " + Byte.MIN_VALUE); //Minimum value of Byte: -128
+		System.out.println("Maximum value of Short : " +Short.MAX_VALUE); //Maximum value of Short : 32767
+		System.out.println("Minimum value of Short: " +Short.MIN_VALUE); //Minimum value of Short: -32768
+		System.out.println("Maximum value of Integer : " +Integer.MAX_VALUE); //Maximum value of Integer : 2147483647
+		System.out.println("Minimum value of Integer: " +Integer.MIN_VALUE); //Minimum value of Integer: -2147483648
 	}
 
 }
