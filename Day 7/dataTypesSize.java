cf2c774a3458f1e3ffe8a0b6b4bde8d9ac4b9a22
Vishnupriya609 <vishnupriyapallerla@gmail.com>
2026-05-25 12:09:29 +0530

    Add files via upload

---
+++ Day 7/dataTypesSize.java
@@ -0,0 +1,14 @@
+// in programmatic to find size
+public class dataTypesSize {
+
+	public static void main(String[] args) {
+		
+		// using wrapper classes
+		System.out.println("Short Size(bits) : " + Short.SIZE);
+		System.out.println("Float Size(bytes) : " + Float.SIZE/8);
+		System.out.println("Integer Size(bits) : " + Integer.SIZE/8);
+		
+
+	}
+
+}
