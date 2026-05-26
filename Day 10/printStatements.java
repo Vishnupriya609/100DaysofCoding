635c850f52d12decbb4068e800b0568f40fb2a5a
Vishnupriya609 <vishnupriyapallerla@gmail.com>
2026-05-26 14:10:40 +0530

    Add files via upload

---
+++ Day 10/printStatements.java
@@ -0,0 +1,28 @@
+package com.printStatements;
+
+public class printStatements {
+
+	public static void main(String[] args) {
+		// print statement 
+		System.out.print("Hello,I am Vishnu"); //print texts in the same line
+		System.out.print(" Can we learn java together?");
+		
+		//println statement
+		System.out.println("Yes,we can learn java together"); // prints output & goes to the new line
+		System.out.println("From java Basics");
+		
+
+		//outputs in red & prints the error message in the console 
+		System.err.println("Error Message");
+
+		//printf statement
+		System.out.printf("The Current year is %d & the month is %s " , 2022,"March");
+		
+		
+		
+		//outputs in red & prints the error message in the console 
+		System.err.println("Error Message");
+
+	}
+
+}
